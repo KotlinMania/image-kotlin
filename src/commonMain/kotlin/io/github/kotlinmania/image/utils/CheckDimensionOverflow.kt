3cbd7259520aefab10109d92d55f370e2a29b072
Sydney Renee <sydney@solace.ofharmony.ai>
2026-08-23 19:44:09 -0700

    chore(infra): unify utils module, fix test port-lint headers, update security resolutions and SPM harness

--- src/commonMain/kotlin/io/github/kotlinmania/image/utils/CheckDimensionOverflow.kt
+++
@@ -1,6 +0,0 @@
-// port-lint: source utils/mod.rs
-package io.github.kotlinmania.image.utils
-
-/** Checks if the provided dimensions would cause an overflow. */
-internal fun checkDimensionOverflow(width: UInt, height: UInt, bytesPerPixel: UByte): Boolean =
-    width.toULong() * height.toULong() > ULong.MAX_VALUE / bytesPerPixel.toULong()
